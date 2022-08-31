287c41e0b12518a0c12a949060340e147cb6b859
TomofumiUyama <u.tommy9014@gmail.com>
2022-08-31 23:21:06 +0900

    first commit

---
+++ SerialCommunicationWPF/SerialCommunicationWPF/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SerialConnectWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Port p = new Port();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            btnOpen.IsEnabled = true;
+            btnSend.IsEnabled = false;
+            cmbComPort.ItemsSource = p.GetPortNum();
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            if (cmbComPort.Text == "") return;
+
+            if (p.PortOpen(cmbComPort.Text))
+            {
+                btnSend.IsEnabled = true;
+            }
+            else
+            {
+                btnSend.IsEnabled = false;
+            }
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            btnSend.IsEnabled = false;
+
+            if (txtSendData.Text == "") return;
+
+            p.Send(txtSendData.Text);
+            //txtLog.Text = await p.ReciveAsync();
+
+            btnSend.IsEnabled = true;
+        }
+    }
+}
---
+++ SerialCommunicationWPF/SerialCommunicationWPF/Port.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Windows;
+
+namespace SerialConnectWPF
+{
+    class Port
+    {
+        private  SerialPort _serialPort = null;
+
+        public bool PortOpen(string comNum)
+        {
+            _serialPort ??= new SerialPort
+            {
+                PortName = comNum,
+                BaudRate = 9600,
+                DataBits = 8,
+                Parity = Parity.None,
+                StopBits = StopBits.One,
+                Handshake = Handshake.None,
+                Encoding = Encoding.UTF8,
+                WriteTimeout = 100000,
+                ReadTimeout = 100000,
+                NewLine = "\r\n",
+            };
+
+            // データ受信イベント設定
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(EventReciveAsync);
+
+            if (!_serialPort.IsOpen)
+            {
+                try
+                {
+                    _serialPort.Open();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        /// <summary>
+        /// ポート切断
+        /// _serialPortがnull状態で実行するとエラーになるのでnull条件演算子で確認する
+        /// </summary>
+        public void PortClose()
+        {
+            _serialPort?.Close();
+            _serialPort = null;
+        }
+
+
+        /// <summary>
+        /// 電文送信
+        /// </summary>
+        /// <param name="serialTxt"></param>
+        public void Send(string serialTxt)
+        {
+            // COMポートがオープン状態じゃないとエラーになるので確認し、閉じている場合は戻す
+            if (!_serialPort.IsOpen) return;
+
+            try
+            {
+                var tmp = serialTxt.Split("-").Select(hex => Convert.ToByte(hex, 16));
+                byte[] sendData = tmp.ToArray();
+
+                // データ書き込み
+                _serialPort.Write(sendData, 0, sendData.Length);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        /// <summary>
+        ///  データ受信用イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void EventReciveAsync(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (!_serialPort.IsOpen) return;
+
+            var srtDataReceived = "";
+            var buf = new byte[_serialPort.BytesToRead];
+
+            try
+            {
+                _serialPort.Read(buf, 0, buf.Length);
+                srtDataReceived = BitConverter.ToString(buf);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            if (_serialPort.IsOpen)
+            {
+                MessageBox.Show(srtDataReceived);
+            }
+        }
+
+
+        /// <summary>
+        /// PCが認識しているCOM番号の一覧を取得する
+        /// </summary>
+        /// <returns></returns>
+        public string[] GetPortNum()
+        {
+            return SerialPort.GetPortNames();
+        }
+
+    }
+}
